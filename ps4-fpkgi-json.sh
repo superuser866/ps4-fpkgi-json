7eedee4a05e0a85cd3853f119a335978abc978ca
superuser866 <118551308+superuser866@users.noreply.github.com>
2025-03-06 09:13:08 +0100

    Update ps4-fpkgi-json.sh
    - Added "DATA" node for better compatibility
    - Added region extraction
    - Fixed version extraction
    - Fixed icon extraction

--- ps4-fpkgi-json.sh
+++ ps4-fpkgi-json.sh
@@ -27,11 +27,13 @@ update_json() {
     local key="$2"
     local value="$3"
     
+    # Create a new file if it doesn't exist
     if [ ! -f "$json_file" ]; then
-        echo "{}" > "$json_file"
+        echo '{"DATA": {}}' > "$json_file"
     fi
     
-    jq --arg k "$key" --argjson v "$value" '. + {($k): $v}' "$json_file" > tmp.json && mv tmp.json "$json_file"
+    # Update the JSON file by adding the new value to the "DATA" block
+    jq --arg k "$key" --argjson v "$value" '.DATA += {($k): $v}' "$json_file" > tmp.json && mv tmp.json "$json_file"
 }
 
 # Function to check if a PKG is already listed in a JSON
@@ -43,7 +45,7 @@ pkg_exists_in_json() {
     
     # If found, return true (0) else false (1)
     if [ "$result" -gt 0 ]; then
-	return 0
+        return 0
     else
         return 1 
     fi
@@ -51,49 +53,50 @@ pkg_exists_in_json() {
 
 cleanup_json() {
     local json_file="$1"
-    cJsonRm=0
+
+    # Se il file JSON non esiste o è vuoto, esci
+    if [ ! -f "$json_file" ] || [ ! -s "$json_file" ]; then
+        echo "JSON file $json_file not found or empty. Skipping cleanup."
+        return
+    fi
 
     # Read keys (PKG names) from JSON
-    original_keys=$(jq -r 'keys[]' "$json_file")
+    original_keys=$(jq -r '.DATA | keys[]' "$json_file")
 
     kept_keys=""
     deleted_keys=""
 
     # For every key (PKG name) in the JSON, checks if file exists
     while IFS= read -r key; do
-	# Removes URL from path
-	full_path=${key#$SERVER_URL}
+        full_path=${key#$SERVER_URL}  # Rimuove la parte dell'URL per ottenere il percorso del file
 
         if [ -f "$full_path" ]; then
-            #echo "File found: $full_path"
-            kept_keys+="$key"$'\n'  
+            kept_keys+="$key"$'\n'
         else
-            echo "Record deleted (not found) in $1 : $full_path"
+            echo "Record deleted (not found) in $json_file: $full_path"
             deleted_keys+="$key"$'\n'
-	    #cJsonRm = $((cJsonRm + 1))
         fi
     done <<< "$(echo "$original_keys")"
 
     # Removes invalid records from JSON
     jq --argjson kept_keys "$(echo "$kept_keys" | jq -R -s -c 'split("\n") | map(select(length > 0))')" '
-        to_entries | 
-        map(select(.key as $key | $kept_keys | index($key))) | 
-        from_entries' "$json_file" > tmp.json && mv tmp.json "$json_file"
+        {DATA: ( .DATA | to_entries | map(select(.key as $key | $kept_keys | index($key))) | from_entries )}' \
+        "$json_file" > tmp.json && mv tmp.json "$json_file"
 
-    #echo "PKGs removed from $1 -> $cJsonRm"
+    echo "Cleanup completed for $json_file"
 }
+
 # Create json files if they dont exist
 if [ ! -f "$JSON_GAMES" ]; then
-	echo "{}" > "$JSON_GAMES"
+    echo '{"DATA": {}}' > "$JSON_GAMES"
 fi
 if [ ! -f "$JSON_UPDATES" ]; then
-	echo "{}" > "$JSON_UPDATES"
+    echo '{"DATA": {}}' > "$JSON_UPDATES"
 fi
 if [ ! -f "$JSON_DLC" ]; then
-	echo "{}" > "$JSON_DLC"
+    echo '{"DATA": {}}' > "$JSON_DLC"
 fi
 
-# Avvia il container Docker
 echo "Starting OpenOrbis Docker container..."
 container_id=$(docker run --rm -dit --name "$CONTAINER_NAME" -w /workspace -u $(id -u):$(id -g) -v "$(realpath "$INPUT_DIR")":/workspace openorbisofficial/toolchain)
 
@@ -104,10 +107,10 @@ while read -r pkg; do
     pkg_dir=$(dirname "$pkg")
     
     # Check if pkg is already in jsons
-	if pkg_exists_in_json "$pkg_name" "$JSON_GAMES" || pkg_exists_in_json "$pkg_name" "$JSON_UPDATES" || pkg_exists_in_json "$pkg_name" "$JSON_DLC"; then
-	    echo "Skip: $pkg_name already listed in JSONs."
-	    continue
-	fi
+    if pkg_exists_in_json "$pkg_name" "$JSON_GAMES" || pkg_exists_in_json "$pkg_name" "$JSON_UPDATES" || pkg_exists_in_json "$pkg_name" "$JSON_DLC"; then
+        echo "Skip: $pkg_name already listed in JSONs."
+        continue
+    fi
 	
     # Check if pkg_dir is subdir of path
     if [[ "$pkg_dir" == "$INPUT_DIR"* ]]; then
@@ -143,10 +146,22 @@ while read -r pkg; do
     category=$(docker exec "$CONTAINER_NAME" grep "CATEGORY" /workspace/tmpfile | awk -F'=' '{print $2}' | tr -d ' ')
     title_id=$(docker exec "$CONTAINER_NAME" grep "TITLE_ID" /workspace/tmpfile | awk -F'=' '{print $2}' | tr -d ' ')
     title=$(docker exec "$CONTAINER_NAME" grep "TITLE " /workspace/tmpfile | awk -F'=' '{print $2}' | sed 's/^ *//;s/ *$//')    
-    version=$(docker exec "$CONTAINER_NAME" grep "VERSION" /workspace/tmpfile | awk -F'=' '{print $2}' | tr -d ' ')
+    version=$(docker exec "$CONTAINER_NAME" grep "APP_VER" /workspace/tmpfile | awk -F'=' '{print $2}' | tr -d ' ')
     release_tmp=$(docker exec "$CONTAINER_NAME" grep "PUBTOOLINFO" /workspace/tmpfile | grep -o "c_date=[0-9]*" | cut -d'=' -f2)
     release=$(echo "$release_tmp" | sed 's/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)/\2-\3-\1/')
     size=$(stat -c %s "$pkg")
+    content_id=$(docker exec "$CONTAINER_NAME" grep "CONTENT_ID" /workspace/tmpfile | awk -F'=' '{print $2}' | tr -d ' ')
+    region="${content_id:0:1}"
+    	if [[ "$region" == "J" ]]; then 
+		region="JAP"
+	elif [[ "$region" == "E" ]]; then
+		region="EUR"
+	elif [[ "$region" == "U" ]]; then
+		region="USA"
+	else 
+		region="null"
+	fi
+
     cover_url="$SERVER_URL"
     cover_url+="_img/$title_id.png"
     pkg_url="$SERVER_URL$pkg_name"
@@ -154,28 +169,23 @@ while read -r pkg; do
 	    coverexists=1
 	else
 	    coverexists=0
-	    pic1_index=$(docker exec "$CONTAINER_NAME" grep 'PIC0_PNG' /workspace/tmpfile1 | awk '{print $4}')
-	    # If PIC0 is empty, try PIC1
+	    pic1_index=$(docker exec "$CONTAINER_NAME" grep 'ICON0_PNG' /workspace/tmpfile1 | awk '{print $4}')
+	    #echo "pic1_index: $pic1_index"
+	    # If ICON0 is empty, try PIC0
 	    if [[ -z "$pic1_index" ]]; then
-	        pic1_index=$(docker exec "$CONTAINER_NAME" grep 'PIC1_PNG' /workspace/tmpfile1 | awk '{print $4}')
+	        pic1_index=$(docker exec "$CONTAINER_NAME" grep 'PIC0_PNG' /workspace/tmpfile1 | awk '{print $4}')
 	    fi
 	fi
+
     echo "========================="
-    echo "TITLE_ID: $title_id"
-    echo "TITLE: $title"
-    echo "CATEGORY: $category"
-    echo "VERSION: $version"
-    echo "RELEASE: $release"
-    echo "SIZE: $size"
-    echo "PKG_URL: $pkg_url"
-    echo "COVER_URL: $cover_url"
-    json_entry=$(jq -n --arg title_id "$title_id" --arg name "$title" --arg version "$version" \
-                      --arg release "$release" --arg size "$size" --arg cover_url "$cover_url" \
-                      '{title_id: $title_id, name: $name, version: $version, release: $release, size: ($size | tonumber), cover_url: $cover_url}')
-    
+    # Create json entry for the element
+    json_entry=$(jq -n --arg title_id "$title_id" --arg region "$region" --arg name "$title" --arg version "$version" \
+                      --arg release "$release" --argjson size $size --arg cover_url "$cover_url" \
+                      '{title_id: $title_id, region: $region, name: $name, version: $version, release: $release, size: $size, cover_url: $cover_url}')
+
     case "$category" in
         "gd") 
-            #echo "Category: Game"
+	    echo "CATEGORY: GAME"            
 	    if [[ $coverexists -eq 0 ]]; then
   		docker exec "$CONTAINER_NAME" /lib/OpenOrbisSDK/bin/linux/PkgTool.Core pkg_extractentry "/workspace/$pkg_name" "$pic1_index" "/workspace/_img/$title_id.PNG"
             	#echo "Extracted cover .$IMG_SUBDIR/$title_id.PNG"
@@ -184,17 +194,27 @@ while read -r pkg; do
 	    cGames=$((cGames + 1))
             ;;
         "gp") 
-            #echo "Category: Update"
+            echo "CATEGORY: UPDATE"
             update_json "$JSON_UPDATES" "$pkg_url" "$json_entry"
             cUpd=$((cUpd + 1))
             ;;
         "ac") 
-            #echo "Category: DLC"
+            echo "CATEGORY: DLC"
             update_json "$JSON_DLC" "$pkg_url" "$json_entry"
             cDlc=$((cDlc + 1))
             ;;
     esac
-   #remove tmp files
+
+    echo "TITLE_ID: $title_id"
+    echo "REGION: $region"
+    echo "TITLE: $title"
+    echo "VERSION: $version"
+    echo "RELEASE: $release"
+    echo "SIZE: $size"
+    echo "PKG_URL: $pkg_url"
+    echo "COVER_URL: $cover_url"
+
+   #Remove tmp files
     docker exec "$CONTAINER_NAME" rm -f "$sfo_file" /workspace/tmpfile /workspace/tmpfile1
 
 done < <(find "$INPUT_DIR" -type f -name "*.pkg")
