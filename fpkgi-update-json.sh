239ec194a0a0c16914d35038c31a06d7b6d0b4f8
superuser866 <118551308+superuser866@users.noreply.github.com>
2025-04-23 10:47:26 +0200

    Add files via upload

---
+++ fpkgi-update-json.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+UPDATE_URL="http://odin.lan/PS4/"
+MONITOR_DIR="/nfs/PS4/Games"
+
+# File update request
+UPDATE_REQUEST_FILE="/tmp/fpkgi_update_request"
+LOCK_FILE="/tmp/fpkgi-update-json.lock"
+
+# Print with timestamp
+echo_ts() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+# If script is still running, exit
+if [ -f "$LOCK_FILE" ]; then
+    echo_ts "Script is already running. Exiting."
+    exit 1
+fi
+
+# Crea il file di lock
+touch "$LOCK_FILE"
+trap "rm -f $LOCK_FILE" EXIT
+
+
+# Check the request file
+if [ -f "$UPDATE_REQUEST_FILE" ]; then
+    # Print timestamp from file
+    LAST_UPDATE_TIMESTAMP=$(cat "$UPDATE_REQUEST_FILE")
+    echo_ts "Request file found. Timestamp: $LAST_UPDATE_TIMESTAMP"
+
+    # Delete request file
+    rm -f "$UPDATE_REQUEST_FILE"
+    echo_ts "File di richiesta aggiornamento json cancellato."
+
+    # Update json
+    echo_ts "Updating json..."
+    cd $MONITOR_DIR
+    ./ps4-fpkgi-json.sh $UPDATE_URL
+#else
+#    echo_ts "Request file not found."
+fi
+
